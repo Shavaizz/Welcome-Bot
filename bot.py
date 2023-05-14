6e7abc4a03a8110c741b9380c0df3eea1bfebdc7
Shavaiz <58118381+Shavaizz@users.noreply.github.com>
2023-05-14 15:57:29 +0000

    Sync Fixed, desc added to commands

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import os
 import random
+from discord.ext.commands import Context
 import aiohttp
 import discord
 from discord.ext import commands
 import datetime
 import asyncio
+from typing import Optional, Literal
+from discord.ext.commands import Greedy
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -17,16 +20,38 @@ formatted_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
 @bot.command()
-async def sync(ctx):
-    """Syncs the cogs and other components"""
-    await ctx.send("Syncing components...")
-    bot.unload_extension("cogs.jokes")
-    bot.load_extension("cogs.jokes")
-    await ctx.send("Components synced successfully.")
+@commands.guild_only()
+@commands.is_owner()
+async def sync(
+  ctx: Context, guilds: Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    if not guilds:
+        if spec == "~":
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "*":
+            ctx.bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "^":
+            ctx.bot.tree.clear_commands(guild=ctx.guild)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
+        else:
+            synced = await ctx.bot.tree.sync()
+        await ctx.send(
+            f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+        )
+        return
 
+    ret = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+        except discord.HTTPException:
+            pass
+        else:
+            ret += 1
+    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
 @bot.listen()
 async def on_message(message):
-    
     username = str(message.author)
     channel = str(message.channel)
     user_message = str(message.content)
@@ -36,9 +61,7 @@ async def on_message(message):
         print(f"{username} said: {user_message} in {channel}")
     if message.author == bot.user:
         return
-    
     messageContent = message.content.lower()
-
     if messageContent == "hello":
         await message.channel.send("Hi,How are you?")
     elif messageContent == "oye":
@@ -54,21 +77,19 @@ async def purge(ctx, *, amt):
     msg = await ctx.send(f"Purged, {amt} messages successfully")
     await asyncio.sleep(3)
     await msg.delete()
-  
-@bot.tree.command()
+@bot.tree.command(description='Makes a random dadjoke')
 async def dadjoke(interaction: discord.Interaction) -> None:
-        async with aiohttp.ClientSession() as session:
-            async with session.get("https://icanhazdadjoke.com", headers={"Accept": "application/json"}) as resp:
-                data = await resp.json()
-                await interaction.response.send_message(data["joke"])
-@bot.tree.command()
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://icanhazdadjoke.com", headers={"Accept": "application/json"}) as resp:
+            data = await resp.json()
+            await interaction.response.send_message(data["joke"])
+@bot.tree.command(description='Makes a random joke')
 async def joke(interaction: discord.Interaction) -> None:
-        async with aiohttp.ClientSession() as session:
-            async with session.get('https://official-joke-api.appspot.com/random_joke') as resp:
-                data = await resp.json()
-                await interaction.response.send_message(f"{data['setup']}\n\n{data['punchline']}")
-
-@bot.tree.command()
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://official-joke-api.appspot.com/random_joke') as resp:
+            data = await resp.json()
+            await interaction.response.send_message(f"{data['setup']}\n\n{data['punchline']}")
+@bot.tree.command(description='Flips a coin to get either heads or tails')
 async def coinflip(interaction: discord.Interaction) -> None:
     if random.choice(determine_flip) == 1:
         embed = discord.Embed(title="Coinflip | (Welcomer Bot)",
@@ -91,9 +112,7 @@ async def on_member_join(member: discord.Member):
     await member.add_roles(role)
     await channel.send(embed=embed)
     print(f"{role}role given to memeber{member.mention}")
-
 @bot.event
 async def on_ready():
-    
     print(f"{bot.user} is now running")
 bot.run(f"{TOKEN}")
