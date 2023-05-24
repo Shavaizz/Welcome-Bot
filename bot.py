90c6b2ac213c7c01cc4647c0020a329b7810c363
Shavaizz <shaviz4444@gmail.com>
2023-05-24 17:25:03 +0500

    Added verification features to bot

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import discord
 from discord.ext import commands
 import datetime
 import asyncio
+from discord import app_commands
 from typing import Optional, Literal
 from discord.ext.commands import Greedy
 from dotenv import load_dotenv
@@ -74,19 +75,19 @@ async def purge(ctx, *, amt):
     msg = await ctx.send(f"Purged, {amt} messages successfully")
     await asyncio.sleep(3)
     await msg.delete()
-@bot.tree.command(description='Makes a random dadjoke')
+@bot.tree.command(name="dadjoke",description='Makes a random dadjoke')
 async def dadjoke(interaction: discord.Interaction) -> None:
     async with aiohttp.ClientSession() as session:
         async with session.get("https://icanhazdadjoke.com", headers={"Accept": "application/json"}) as resp:
             data = await resp.json()
             await interaction.response.send_message(data["joke"])
-@bot.tree.command(description='Makes a random joke')
+@bot.tree.command(name="joke",description='Makes a random joke')
 async def joke(interaction: discord.Interaction) -> None:
     async with aiohttp.ClientSession() as session:
         async with session.get('https://official-joke-api.appspot.com/random_joke') as resp:
             data = await resp.json()
             await interaction.response.send_message(f"{data['setup']}\n\n{data['punchline']}")
-@bot.tree.command(description='Flips a coin to get either heads or tails')
+@bot.tree.command(name="coinflip",description='Flips a coin to get either heads or tails')
 async def coinflip(interaction: discord.Interaction) -> None:
     if random.choice(determine_flip) == 1:
         embed = discord.Embed(title="Coinflip | (Welcomer Bot)",
@@ -96,7 +97,29 @@ async def coinflip(interaction: discord.Interaction) -> None:
         embed = discord.Embed(title="Coinflip | (Welcomer Bot)",
                                   description=f"{interaction.user.mention} Flipped coin, we got **Tails**!")
         await interaction.response.send_message(embed=embed)
-@bot.tree.command(description='Throw a pie at someone')
+@bot.tree.command(name="verify",description="Verify A User")
+@app_commands.default_permissions(manage_roles=True)
+async def verify(interaction:discord.Interaction,member:discord.Member,):
+    role = discord.utils.get(member.guild.roles,name="Verified Member")
+    embed=discord.Embed(
+        title="Verification Result",
+        description=f"{member.mention} have been verified successfully",
+        color=0xFF5555,
+        timestamp=datetime.datetime.now()
+    )
+    await member.add_roles(role)
+    await interaction.response.send_message(embed=embed)
+@bot.tree.command(name="unverify",description="Unverify A User")
+@app_commands.default_permissions(manage_roles=True,)
+async def unverify(interaction:discord.Interaction,member:discord.Member):
+    embed=discord.Embed(
+        title="Verification Result",
+        description=f"{member.mention} has been unverified and has been banned",
+        color=0XFF5555
+    )
+    await interaction.response.send_message(embed=embed)
+    await member.ban(reason="User has been unverified and been kicked by WelcomerBot")
+@bot.tree.command(name="pie",description='Throw a pie at someone')
 async def pie(interaction:discord.Interaction, member:discord.Member, message:str) -> None:
   embed= discord.Embed(title=f"User:{member.name} Has a pie for you 🥧",
                     description=f'{member.mention} says {message}',
@@ -110,9 +133,9 @@ async def on_member_join(member: discord.Member):
     embed = discord.Embed(
         description=f"Welcome To The Crew, {member.mention}",
         color=0xFF5555,
-        timestamp=formatted_date_time
+        timestamp=datetime.datetime.now(),
         )
-    role = discord.utils.get(member.guild.roles, name="Member")
+    role = discord.utils.get(member.guild.roles, name="Unverified Member")
     await member.add_roles(role)
     await channel.send(embed=embed)
     print(f"{role}role given to memeber{member.mention}")
